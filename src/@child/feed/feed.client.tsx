519c02fa55600703af026e12e281f9d02c509f83
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2023-11-23 00:46:58 -0700

    Refactor feed page to use useQuery hook

--- src/@child/feed/feed.client.tsx
+++ src/@child/feed/feed.client.tsx
@@ -5,24 +5,25 @@ import { useCurrentPage } from "../../@shared/page";
 import type { FeedRouter } from "./feed.server";
 
 
-
+const client = new FeedRouter();
 
 
 export function FeedPage() {
   const currentPage = useCurrentPage();
 
-  async function load() {
-    const feed = await currentPage.query((query) =>
-      query.feed.getFeed({
-        page: 1,
-      })
-    );
-    console.log(feed);
-  }
+  const query = useQuery({
+    queryKey: ["feed"],
+    queryFn: async () => {
+      const feed = await currentPage.query((query) =>
+        query.feed.getFeed({
+          page: 1,
+        })
+      );
+      return feed;
+    },
+  })
 
-  useEffect(() => {
-    load()
-  }, [])
+
 
   return (
     <div className="w-full flex flex-col flex-1">
@@ -32,6 +33,9 @@ export function FeedPage() {
           got to movie details
         </button>
       </div>
+      {query.isLoading && <div>loading...</div>}
+      {query.isError && <div>error</div>}
+      {query.data && <div>{JSON.stringify(query.data)}</div>}
       <div className="w-full flex items-center border-t">
         <button className="flex-1 p-5 border">down</button>
 
