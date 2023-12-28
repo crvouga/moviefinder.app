9dc9fa99609e0a2dc74ec4ed6f25e946dc3d9d0b
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2023-12-28 12:48:07 -0700

    Add dependencies and update configuration files

--- src/@shared/client.ts
+++ src/@shared/client.ts
@@ -1,9 +1,11 @@
 
 import { initClient, AppRouter } from "@ts-rest/core";
 
+const baseUrl = process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
+
 export const createClient = <T extends AppRouter>(contract: T) => {
     return initClient(contract, {
-        baseUrl: "http://localhost:3000",
+        baseUrl,
         baseHeaders: {}
     })
 } 
--- src/main.server.tsx
+++ src/main.server.tsx
@@ -1,34 +1,77 @@
 import 'isomorphic-fetch'
 import Fastify from 'fastify';
+import fastifyStatic from '@fastify/static';
 import { initServer } from '@ts-rest/fastify';
 import { appRouter } from './app.server';
 import cors from '@fastify/cors'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const s = initServer()
+// 
+// 
+// Server
+// 
+// 
 
 const fastify = Fastify();
+
+// 
+// 
+// CORS
+// 
+// 
+
 fastify.register(cors, { 
   origin: true,
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-  preflightContinue: false,
-  optionsSuccessStatus: 204
 })
+
+// 
+// 
+// API
+// 
+// 
+
+const s = initServer()
 
 const router = appRouter({ s })
 
 fastify.register(s.plugin(router.feed));
 
-const start = async () => {
+
+// 
+// 
+// Static files
+// 
+// 
+
+const rootPath = path.join(__dirname, "..", "dist")
+
+fastify.register(fastifyStatic, {
+  root: rootPath,
+  index: "index.html",
+});
+
+// 
+// 
+// Main
+// 
+// 
+
+const PORT = process.env.PORT ?  Number(process.env.PORT) : 5000;
+const main = async () => {
+  console.log("starting server")
   try {
-    await fastify.listen({ port: 3000 });
-    console.log('server started')
+    await fastify.listen({ port: PORT });
+    console.log(`server started on port ${PORT}`)
   } catch (err) {
     fastify.log.error(err);
+    console.error(err)
     process.exit(1);
   }
 };
 
-start();
+main();
