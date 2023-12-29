bfe6638c12d60e84d8466686ae881a5365aefed1
Chris Vouga <crvouga@gmail.com>
2023-12-29 14:07:17 -0700

    Update server script to use tsx watch

--- src/@child/feed/feed.server.ts
+++ src/@child/feed/feed.server.ts
@@ -53,101 +53,100 @@ export const feedRouter = ({ s }: { s: Server }) => {
 
 const getFeedItems = async ({ page }: { page: number }): Promise<Result<FeedItem[], string>> => {
   const got = await tmdbClient.discover.movie({
-        pathParams: undefined,
-        queryParams: {
-          page: page,
-          include_adult: false,
-          include_video: true,
-        }
-      })
+    pathParams: undefined,
+    queryParams: {
+      page: page,
+      include_adult: false,
+      include_video: true,
+    }
+  })
 
-      
-      if (!got.success) {
-        return Err("Something went wrong")
-      }
-      
-      if (got.data.status !== 200) {
-        return Err(got.data.body.status_message ?? "Something went wrong")
-      }
-      
-      
-      const config = await tmdbClient.configuration({ pathParams: undefined, queryParams: {}, })
+  
+  if (!got.success) {
+    return Err("Something went wrong")
+  }
+  
+  if (got.data.status !== 200) {
+    return Err(got.data.body.status_message ?? "Something went wrong")
+  }
+  
+  const config = await tmdbClient.configuration({ pathParams: undefined, queryParams: {}, })
 
-      if (!config.success || config.data.status !== 200 || !config.data.body.images?.base_url) {
-        return Err("Something went wrong while loading image configuration.")
-        
-      }
+  if (!config.success || config.data.status !== 200 || !config.data.body.images?.base_url) {
+    return Err("Something went wrong while loading image configuration.")
+    
+  }
 
-      const movieVideoResponses = await Promise.all(
-        got.data.body.results.map((movie) => {
-          return tmdbClient.movie.video({
-            pathParams: { movie_id: movie.id },
-            queryParams: {},
-          });
-        })
-      );
-
-      const videosByMovieId = new Map<number, TmdbMovieVideo[]>();
-      for (const response of movieVideoResponses) {
-        if (!response.success) {
-          continue;
-        }
-
-        if (response.data.status !== 200) {
-          continue;
-        }
-
-        const results = response.data?.body?.results;
-
-        if (!results) {
-          continue;
-        }
-
-        const movieId = response.data.body.id;
-
-        if (!movieId) {
-          continue;
-        }
-
-        videosByMovieId.set(movieId, results);
-      }
-
-      const moviesWithVideos = got.data.body.results.map((movie) => {
-        const videos = videosByMovieId.get(movie.id) ?? [];
-        return { ...movie, videos };
+  const movieVideoResponses = await Promise.all(
+    got.data.body.results.map((movie) => {
+      return tmdbClient.movie.video({
+        pathParams: { movie_id: movie.id },
+        queryParams: {},
       });
+    })
+  );
 
-      const tmdbConfig = config.data.body
+  const videosByMovieId = new Map<number, TmdbMovieVideo[]>();
+  for (const response of movieVideoResponses) {
+    if (!response.success) {
+      continue;
+    }
 
-      const feedItems: FeedItem[] = moviesWithVideos.flatMap((result): FeedItem[] => {
-        const posterPath = result.poster_path
+    if (response.data.status !== 200) {
+      continue;
+    }
 
-        const posterUrl = toTmdbImageUrl(
-          tmdbConfig,
-          posterPath
-        )
+    const results = response.data?.body?.results;
 
-        if (!posterUrl) {
-          return []
-        }
-        
-        const thirdPartyVideoUrls = result.videos.flatMap((video) => {
-          const src = toVideoSrc(video);
-          if (!src) {
-            return [];
-          }
-          return [src];
-        })
+    if (!results) {
+      continue;
+    }
+
+    const movieId = response.data.body.id;
+
+    if (!movieId) {
+      continue;
+    }
+
+    videosByMovieId.set(movieId, results);
+  }
+
+  const moviesWithVideos = got.data.body.results.map((movie) => {
+    const videos = videosByMovieId.get(movie.id) ?? [];
+    return { ...movie, videos };
+  });
+
+  const tmdbConfig = config.data.body
+
+  const feedItems: FeedItem[] = moviesWithVideos.flatMap((result): FeedItem[] => {
+    const posterPath = result.poster_path
+
+    const posterUrl = toTmdbImageUrl(
+      tmdbConfig,
+      posterPath
+    )
+
+    if (!posterUrl) {
+      return []
+    }
+    
+    const thirdPartyVideoUrls = result.videos.flatMap((video) => {
+      const src = toVideoSrc(video);
+      if (!src) {
+        return [];
+      }
+      return [src];
+    })
 
 
-        return [{
-          id: result.id.toString(),
-          title: result.title,
-          posterUrl: posterUrl,
-          thirdPartyVideoUrls: thirdPartyVideoUrls,
-        }]
-      })
+    return [{
+      id: result.id.toString(),
+      title: result.title,
+      posterUrl: posterUrl,
+      thirdPartyVideoUrls: thirdPartyVideoUrls,
+    }]
+  })
 
 
-    return Ok(feedItems)
+  return Ok(feedItems)
 }
