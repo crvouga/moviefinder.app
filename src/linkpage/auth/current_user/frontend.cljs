c2933fc5a2c4c5c41c1502e1180ec1e55afa57e7
Chris Vouga <crvouga@gmail.com>
2025-01-16 01:32:31 -0700

    Refactor state transition handling by replacing defmulti with store registration for improved clarity and maintainability

--- src/linkpage/auth/current_user/frontend.cljs
+++ src/linkpage/auth/current_user/frontend.cljs
@@ -1,27 +1,23 @@
 (ns linkpage.auth.current-user.frontend
   (:require [linkpage.frontend.store :as store]))
 
-(defmulti transition store/msg-type)
+(store/reg!
+ :store/initialized
+ (fn [i]
+   (-> i
+       (update-in [:store/state] assoc ::current-user [:result/loading])
+       (update-in [:store/effs] conj [:rpc/send! {:rpc/req [:current-user/get]
+                                                  :rpc/res #(vector ::got-current-user %)}])))
+ 
+ :login/authenticated
+ (fn [i]
+   (-> i
+       (assoc-in [:store/state ::current-user] (store/msg-payload i))))
 
-(store/register-transition! transition)
-
-(defmethod transition :default [i] i)
-
-(defmethod transition :store/initialized [i]
-  (-> i
-      (update-in [:store/state] assoc ::current-user [:result/loading])
-      (update-in [:store/effs] conj [:rpc/send! {:rpc/req [:current-user/get]
-                                                 :rpc/res #(vector ::got-current-user %)}])))
-
-(defmethod transition ::got-current-user [i]
-  (-> i
-      (assoc-in [:store/state ::current-user] (store/msg-payload i))))
-
-#_(defmethod transition :login/authenticated [i]
-    (-> i
-        (assoc-in [:store/state ::current-user] (store/msg-payload i))))
-
-(defmulti view (fn [i] (-> i :store/state ::current-user first)))
+ ::got-current-user
+ (fn [i]
+   (-> i
+       (assoc-in [:store/state ::current-user] (store/msg-payload i)))))
 
 (defn view-centered-message [message]
   [:div {:style {:display "flex"
@@ -30,6 +26,8 @@
                  :width "100vw"
                  :height "100vh"}}
    [:strong message]])
+
+(defmulti view (fn [i] (-> i :store/state ::current-user first)))
 
 (defmethod view :result/not-asked [_i]
   [view-centered-message "Not asked"])
