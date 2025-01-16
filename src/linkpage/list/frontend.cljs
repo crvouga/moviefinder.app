07edbbdede05ad2c2c19fa20b989e945baa462c4
Chris Vouga <crvouga@gmail.com>
2025-01-16 01:37:25 -0700

    Add initial implementation for querying and viewing liked movies

---
+++ src/linkpage/list/frontend.cljs
@@ -0,0 +1,20 @@
+(ns linkpage.list.frontend)
+
+
+(defn query-my-liked-movies [input]
+  {:select [:movie/id :movie/title :movie/year :movie/plot :movie/poster]
+   :from [:movie]
+   :join [:movie/like]
+   :where [:=  (:user/id input)]})
+
+#_(store/sub!
+ ::lists
+ (fn [i]
+   {:select [:movie/id :movie/title :movie/year :movie/plot :movie/poster]
+    :from [:movie]
+    :join [:movie/like]
+    :where [:=  (:user/id i)]}))
+
+(defn view [i]
+  (let [lists (-> i :store/state ::lists)]
+    [:div ]))
