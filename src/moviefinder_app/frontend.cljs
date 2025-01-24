7407d7509e905a77337722aca9b008f08372c526
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2025-01-23 17:16:05 -0700

    Refactor package.json scripts for improved build process and update frontend styling for better UI consistency

--- src/moviefinder_app/frontend.cljs
+++ src/moviefinder_app/frontend.cljs
@@ -13,7 +13,7 @@
 
 (defn view-root [i]
   [:div {:class "fixed left-1/2 top-1/2 flex h-[100dvh] w-screen -translate-x-1/2 -translate-y-1/2 items-center justify-center overflow-hidden bg-black text-white"}
-   [:div {:class "relative flex h-full max-h-[915px] w-full max-w-[520px] flex-col items-center justify-center overflow-hidden rounded min-[520px]:border border-neutral-800"}
+   [:div {:class "relative flex h-full max-h-[915px] w-full max-w-[520px] flex-col items-center justify-center overflow-hidden rounded min-[520px]:border border-neutral-500"}
     (toaster/view i)
     (current-user/view-guard i screen/view)]])
 
