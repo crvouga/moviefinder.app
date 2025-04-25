e9a3f473adddf8ae3fe425a3dc2efc890e5bfb3c
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2025-04-25 12:47:36 -0700

    Refactor mod.cljs to change mods from a set to a map for improved registration and retrieval of modules. Update registration logic to use assoc for key-value pairs and adjust view function to ensure unique keys for rendering components.

--- src/app/frontend/mod.cljs
+++ src/app/frontend/mod.cljs
@@ -1,19 +1,19 @@
 (ns app.frontend.mod)
 
-(defonce ^:private mods (atom #{}))
+(defonce ^:private mods (atom {}))
 
 (defn reg [mod]
   (println "register" mod)
-  (swap! mods conj mod))
+  (swap! mods assoc (:mod/name mod) mod))
 
 (defn logic [i]
-  (doseq [{:keys [mod/logic-fn]} @mods]
+  (doseq [{:keys [mod/logic-fn]} (vals @mods)]
     (when (fn? logic-fn)
       (logic-fn i))))
 
 (defn view [i]
   [:<>
-   (for [{:keys [mod/view-fn mod/name]} @mods
+   (for [[mod-name {:keys [mod/view-fn]}] @mods
          :when (fn? view-fn)]
-     ^{:key (str name)}
+     ^{:key mod-name}
      [view-fn i])])
