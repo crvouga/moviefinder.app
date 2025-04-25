75f17563917b9aabe4fc38d231f37b8db3a8d9e8
Chris Vouga <crvouga@gmail.com>
2025-04-24 17:34:22 -0700

    Add mutation observer utilities for DOM changes
    Implement `mutation-observer-chan!` to create a channel for DOM mutation notifications and `query-chan!` to observe changes for specific elements. These additions enhance the ability to react to dynamic DOM updates in the application.

--- src/core/dom.cljs
+++ src/core/dom.cljs
@@ -5,6 +5,32 @@
 (defn query! [css-selector]
   (js/document.querySelector css-selector))
 
+(defn mutation-observer-chan!
+  "Creates a channel that receives notifications when DOM mutations occur.
+   Returns a channel that will receive the MutationRecord objects."
+  [target-node config]
+  (let [result-chan (a/chan)
+        observer (js/MutationObserver.
+                  (fn [mutations _observer]
+                    (a/put! result-chan mutations)))]
+    (.observe observer target-node (clj->js config))
+    result-chan))
+
+(defn query-chan! [css-selector]
+  (let [result-chan (a/chan)
+        observer (js/MutationObserver.
+                  (fn [_]
+                    (when-let [element (query! css-selector)]
+                      (a/put! result-chan element))))
+        config #js {:childList true
+                    :subtree true
+                    :attributes true}]
+    ;; Check immediately in case the element already exists
+    (when-let [element (query! css-selector)]
+      (a/put! result-chan element))
+    (.observe observer js/document.body config)
+    result-chan))
+
 (defn event-chan
   "Creates a channel that listens for DOM events on a given DOM node.
    Returns a channel that will receive events when they occur."
