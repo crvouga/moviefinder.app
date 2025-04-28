afde505b81cdf680de6341c1a2c919fb1942bb6c
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2025-04-28 13:16:42 -0700

    Add Clojure.spec validations for program structure and enhance program functions
    - Introduced Clojure.spec definitions for program state, message channel, and effect/reducer functions.
    - Updated `new` function to assert program validity upon creation.
    - Added specs for `reducer`, `state!`, `eff!`, `reg-eff`, `put!`, `take!`, and `take-every!` functions to ensure correct argument types and return values.
    - Refactored `reg-eff` and `put!` functions to return the updated program for better chaining of operations.

--- src/core/program.cljs
+++ src/core/program.cljs
@@ -1,7 +1,30 @@
 (ns core.program
   (:require
    [clojure.core.async :as a]
-   [clojure.pprint :as pprint]))
+   [clojure.pprint :as pprint]
+   [clojure.spec.alpha :as s]))
+
+(s/def ::state! (s/and #(instance? js/Atom %) #(map? @%)))
+(s/def ::msg-chan! #(instance? js/ManyToManyChannel %))
+(s/def ::msg-mult! #(instance? js/Mult %))
+(s/def ::eff-fns! (s/and #(instance? js/Atom %) #(map? @%)))
+(s/def ::reducer-fns! (s/and #(instance? js/Atom %) #(map? @%)))
+
+(s/def ::program
+  (s/keys :req-un [::state!
+                   ::msg-chan!
+                   ::msg-mult!
+                   ::eff-fns!
+                   ::reducer-fns!]))
+
+(s/def ::msg
+  (s/or :simple-msg (s/cat :type keyword?)
+        :complex-msg (s/cat :type keyword? :payload map?)))
+
+
+(defn program? [input]
+  (s/valid? ::program input))
+
 
 (defn new
   "New is a function that takes no arguments. It returns a program. new"
@@ -10,28 +33,27 @@
         msg-chan! (a/chan)
         msg-mult! (a/mult msg-chan!)
         eff-fns! (atom {})
-        reducer-fns! (atom {})]
-    {:program/state! state!
-     :program/msg-chan! msg-chan!
-     :program/msg-mult! msg-mult!
-     :program/eff-fns! eff-fns!
-     :program/reducer-fns! reducer-fns!}))
-
-(defn program? [input]
-  (and (map? input)
-       (:program/state! input)
-       (:program/msg-chan! input)
-       (:program/msg-mult! input)
-       (:program/eff-fns! input)
-       (:program/reducer-fns! input)))
+        reducer-fns! (atom {})
+        program {:program/state! state!
+                 :program/msg-chan! msg-chan!
+                 :program/msg-mult! msg-mult!
+                 :program/eff-fns! eff-fns!
+                 :program/reducer-fns! reducer-fns!}]
+    (assert (program? program) "Program must satisfy program spec")
+    program))
 
 
-(defn reducer
+
+(defn- reducer
   "Reducer is a function that takes a program, a state, and a message. It returns a state. new"
   [program state msg]
   (let [{:keys [program/reducer-fns!]} program
         reducer-fn (get @reducer-fns! (first msg) (constantly state))]
     (reducer-fn state msg)))
+
+(s/fdef reducer
+  :args (s/cat :program ::program :state map? :msg ::msg)
+  :ret map?)
 
 (defn reg-reducer
   "Reg-reducer is a function that takes a program, a message type, and a reducer function. It returns a program. new"
@@ -41,9 +63,18 @@
     (swap! reducer-fns! assoc msg-type reducer-fn)))
 
 
+(s/fdef state!
+  :args (s/cat :program ::program)
+  :ret ::state!)
+
 (defn state! [program]
   (let [{:keys [program/state!]} program]
     @state!))
+
+
+(s/fdef eff!
+  :args (s/cat :program ::program :eff ::msg)
+  :ret map?)
 
 (defn eff!
   "Eff is a function that takes a program, and a message. It returns a state. new"
@@ -54,13 +85,22 @@
         eff-fn! (or maybe-eff-fn! (constantly nil))]
     (eff-fn! eff)))
 
+(s/fdef reg-eff
+  :args (s/cat :program ::program :eff-type keyword? :eff-fn fn?)
+  :ret ::program)
+
 (defn reg-eff
   "Reg-eff is a function that takes a program, a message type, and an effect function. It returns a program. new"
   [program eff-type eff-fn]
   (pprint/pprint {:reg-eff eff-type})
   (let [{:keys [program/eff-fns!]} program]
-    (swap! eff-fns! assoc eff-type eff-fn)))
+    (swap! eff-fns! assoc eff-type eff-fn)
+    program))
 
+
+(s/fdef put!
+  :args (s/cat :program ::program :msg ::msg)
+  :ret ::program)
 
 (defn put!
   "Put is a function that takes a program, and a message. It returns a program. new"
@@ -74,17 +114,18 @@
     (pprint/pprint {:put! (if (> (count (str msg)) 1000)
                             (str (first msg) " ... (truncated)")
                             msg)})
-    (a/put! msg-chan! msg)))
+    (a/put! msg-chan! msg)
+    program))
+
+(s/fdef take!
+  :args (s/cat :program ::program :msg-type keyword?)
+  :ret ::msg)
 
 (defn take!
   "Take is a function that takes a program, and a message type. It returns a message. new"
   [program msg-type]
-
-  (when (not (program? program))
-    (throw (js/Error. (str "program must be a program: " program))))
-
-  (when (not (keyword? msg-type))
-    (throw (js/Error. (str "msg-type must be a keyword: " msg-type))))
+  (assert (program? program) "Program must satisfy program spec")
+  (assert (keyword? msg-type) "msg-type must be a keyword")
 
   (let [{:keys [program/msg-mult!]} program
         ch (a/chan)]
@@ -98,6 +139,9 @@
             msg)
           (recur ch))))))
 
+(s/fdef take-every!
+  :args (s/cat :program ::program :msg-type keyword? :f fn?)
+  :ret ::program)
 
 (defn take-every!
   "Take-every is a function that takes a program, a message type, and a function. It returns a program. new"
@@ -106,6 +150,7 @@
     (let [msg (a/<! (take! program msg-type))]
       (f msg)
       (recur))))
+
 
 (defn take-latest!
   "Take-latest is a function that takes a program, and a message type. It returns a message. new"
