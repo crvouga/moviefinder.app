a480dafdb231ba2c45a25f19762dfe3ab2ae3467
Chris Vouga <crvouga@gmail.com>
2025-05-01 02:27:09 -0700

    Remove debug print statements from backend.cljs to clean up authentication flow

--- src/app/auth/current_user/backend.cljs
+++ src/app/auth/current_user/backend.cljs
@@ -10,8 +10,6 @@
  (fn [{:keys [session/session-id] :as req}]
    (a/go
      (a/<! (a/timeout 1000))
-     (println "session-id" session-id)
      (let [{:keys [session/user-id]} (a/<! (session-db/find-by-session-id! req session-id))]
-       (println "user-id" user-id)
        (a/<! (user-db/find-by-user-id! req user-id))))))
 
