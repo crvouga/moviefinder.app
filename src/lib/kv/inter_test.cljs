e4803963e2f621a393458eb085ef406c29a27b11
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2025-05-08 16:07:32 -0700

    Update key-value structure in kv.edn and adjust test cases in inter_test.cljs
    - Added new key-value pairs in kv.edn for `:kv-test/ns-1` and `:kv-test/ns-2` with corresponding data values.
    - Updated variable names in inter_test.cljs to reflect the new `:kv-test/data` structure, ensuring consistency in key-value operations.
    - Modified assertions in tests to check for `:kv-test/data` instead of `:data` after zapping values.

--- src/lib/kv/inter_test.cljs
+++ src/lib/kv/inter_test.cljs
@@ -14,7 +14,7 @@
              :kv/namespace namespace})))
 
 (def k "some-key")
-(def v {:data "some-value"})
+(def v {:kv-test/data "some-value"})
 
 (deftest kv-ops
   (testing "Basic KV operations"
@@ -34,7 +34,7 @@
 
          (let [got (<! (kv/get! kv k))]
            (is (result/ok? got) "Getting after zap should still return a ok result")
-           (is (nil? (:data got)) "Value should be nil after being zapped"))
+           (is (nil? (:kv-test/data got)) "Value should be nil after being zapped"))
 
          (done))))))
 
@@ -43,35 +43,36 @@
     (async
      done
      (go
-       (let [kv-store-1 (new-kv :namespace-1)
-             kv-store-2 (new-kv :namespace-2)
+       (let [kv-1 (new-kv :kv-test/ns-1)
+             kv-2 (new-kv :kv-test/ns-2)
              k "same-key"
-             v-1 {:data "value-1"}
-             v-2 {:data "value-2"}]
+             v-1 {:kv-test/data "value-1"}
+             v-2 {:kv-test/data "value-2"}]
 
-         (<! (kv/set! kv-store-1 k v-1))
-         (<! (kv/set! kv-store-2 k v-2))
+         (<! (kv/set! kv-1 k v-1))
+         (<! (kv/set! kv-2 k v-2))
 
-         (let [get-result-1 (<! (kv/get! kv-store-1 k))
-               get-result-2 (<! (kv/get! kv-store-2 k))]
+         (let [got-1 (<! (kv/get! kv-1 k))
+               got-2 (<! (kv/get! kv-2 k))]
 
-           (is (= v-1 (dissoc get-result-1 :result/type))
+           (is (= v-1 (dissoc got-1 :result/type))
                "Value in namespace 1 should be preserved")
-           (is (= v-2 (dissoc get-result-2 :result/type))
+           (is (= v-2 (dissoc got-2 :result/type))
                "Value in namespace 2 should be preserved")
-           (is (not= (dissoc get-result-1 :result/type)
-                     (dissoc get-result-2 :result/type))
+           (is (not= (dissoc got-1 :result/type)
+                     (dissoc got-2 :result/type))
                "Values in different namespaces should be different"))
 
          (done))))))
-
 
 (deftest kv-string-values
   (testing "KV operations with string values"
     (async
      done
      (go
-       (let [kv (new-kv) k "string-key" v "This is a string value"]
+       (let [kv (new-kv)
+             k "string-key"
+             v "This is a string value"]
          (let [set (<! (kv/set! kv k v))]
            (is (result/ok? set) "Setting a string value should return a ok result"))
 
@@ -84,6 +85,6 @@
 
          (let [got (<! (kv/get! kv k))]
            (is (result/ok? got) "Getting after zap should still return a ok result")
-           (is (nil? (:data got)) "String value should be nil after being zapped"))
+           (is (nil? (:kv-test/data got)) "String value should be nil after being zapped"))
 
          (done))))))
