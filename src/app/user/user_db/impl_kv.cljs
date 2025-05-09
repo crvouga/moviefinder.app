db9d2c6f35ca47eae570dff150910c4e6f857794
Chris Vouga <crvouga@gmail.com>
2025-05-09 02:35:55 -0700

    Refactor asynchronous handling in user database implementation
    - Updated the user database implementation to use `go` directly instead of referencing `clojure.core.async` for cleaner code.
    - Simplified asynchronous calls in `find-by-phone-number!`, `put!`, and `zap!` methods for improved readability and maintainability.

--- src/app/user/user_db/impl_kv.cljs
+++ src/app/user/user_db/impl_kv.cljs
@@ -2,7 +2,7 @@
   (:require
    [app.user.user-db.inter :as user-db]
    [lib.kv.inter :as kv]
-   [clojure.core.async :as a]))
+   [clojure.core.async :refer [<! go]]))
 
 
 (defn- new-by-user-id-kv [kv]
@@ -28,20 +28,20 @@
 
 (defmethod user-db/find-by-phone-number! :user-db/impl-kv
   [{:keys [user-db/by-phone-number-kv] :as inst} phone-number]
-  (a/go
-    (when-let [{:keys [user/user-id]} (a/<! (kv/get! by-phone-number-kv phone-number))]
-      (a/<! (user-db/find-by-user-id! inst user-id)))))
+  (go
+    (when-let [{:keys [user/user-id]} (<! (kv/get! by-phone-number-kv phone-number))]
+      (<! (user-db/find-by-user-id! inst user-id)))))
 
 (defmethod user-db/put! :user-db/impl-kv
   [{:keys [user-db/by-user-id-kv user-db/by-phone-number-kv]}
    {:keys [:user/user-id :user/phone-number] :as user}]
-  (a/go
-    (a/<! (kv/set! by-user-id-kv user-id user))
-    (a/<! (kv/set! by-phone-number-kv phone-number user))))
+  (go
+    (<! (kv/set! by-user-id-kv user-id user))
+    (<! (kv/set! by-phone-number-kv phone-number user))))
 
 (defmethod user-db/zap! :user-db/impl-kv
   [{:keys [user-db/by-user-id-kv user-db/by-phone-number-kv]} user-id]
-  (a/go
-    (when-let [user (a/<! (kv/get! by-user-id-kv user-id))]
-      (a/<! (kv/zap! by-phone-number-kv (:user/phone-number user))))
-    (a/<! (kv/zap! by-user-id-kv user-id))))
+  (go
+    (when-let [user (<! (kv/get! by-user-id-kv user-id))]
+      (<! (kv/zap! by-phone-number-kv (:user/phone-number user))))
+    (<! (kv/zap! by-user-id-kv user-id))))
