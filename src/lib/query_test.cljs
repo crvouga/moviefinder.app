912a183c569293b9bd75484f26ca5ab1b7d3b009
Chris Vouga <crvouga@gmail.com>
2025-05-09 19:48:41 -0700

    Refactor pagination tests and remove commented-out code
    - Removed obsolete test cases for `to-end-page-index` to streamline the test suite.
    - Updated assertions in `to-page-numbers` tests for improved clarity and accuracy.
    - Commented out a test case for `to-page-numbers` to indicate future consideration.

--- src/lib/query_test.cljs
+++ src/lib/query_test.cljs
@@ -21,18 +21,6 @@
     (is (= (query/to-start-page-index {:q/limit (rand-int 20) :q/offset 25} 20) 1)))
 
 
-  #_(testing "to-end-page-index"
-      (is (= (query/to-end-page-index {:q/limit 0 :q/offset 0} 20) 0))
-      (is (= (query/to-end-page-index {:q/limit 0 :q/offset 20} 20) 1))
-      (is (= (query/to-end-page-index {:q/limit 0 :q/offset 40} 20) 2))
-      (is (= (query/to-end-page-index {:q/limit 0 :q/offset 60} 20) 3))
-      (is (= (query/to-end-page-index {:q/limit 0 :q/offset 80} 20) 4))
-      (is (= (query/to-end-page-index {:q/limit 0 :q/offset 5} 20) 0))
-      (is (= (query/to-end-page-index {:q/limit 0 :q/offset 25} 20) 1))
-      ;; 
-      (is (= (query/to-end-page-index {:q/limit 20 :q/offset 10} 20) 1))
-      ;; 
-      )
 
 
   (testing "to-page-indexes"
@@ -48,15 +36,15 @@
 
 
     ;; ;; Edge cases
-    ;; (is (= (query/to-page-numbers {:q/limit 0 :q/offset 0} 20) [1]))
-    ;; (is (= (query/to-page-numbers {:q/limit 1 :q/offset 0} 20) [1]))
-    ;; (is (= (query/to-page-numbers {:q/limit 20 :q/offset 19} 20) [1 2]))
-    ;; (is (= (query/to-page-numbers {:q/limit 100 :q/offset 0} 20) [1 2 3 4 5]))
-    ;; (is (= (query/to-page-numbers {:q/limit 20 :q/offset 999} 20) [50 51]))
+    (is (= (query/to-page-numbers {:q/limit 0 :q/offset 0} 20) []))
+    (is (= (query/to-page-numbers {:q/limit 1 :q/offset 0} 20) [1]))
+    (is (= (query/to-page-numbers {:q/limit 20 :q/offset 19} 20) [1 2]))
+    (is (= (query/to-page-numbers {:q/limit 100 :q/offset 0} 20) [1 2 3 4 5]))
+    (is (= (query/to-page-numbers {:q/limit 20 :q/offset 999} 20) [50 51]))
 
     ;; ;; Different page sizes
     ;; (is (= (query/to-page-numbers {:q/limit 10 :q/offset 0} 5) [1 2]))
-    ;; (is (= (query/to-page-numbers {:q/limit 10 :q/offset 7} 5) [2]))
+    #_(is (= (query/to-page-numbers {:q/limit 10 :q/offset 7} 5) [2]))
 
     ;; (is (= (query/to-page-numbers {:q/limit 100 :q/offset 80} 20) [5]))
     )
